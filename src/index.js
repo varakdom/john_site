e81643f2bbaab768ef38f9d5b0d04799c474cbbc
Dominique <dominique.kambale-baraka@epitech.eu>
2022-12-25 01:41:03 +0100

    [+] functionning site

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import HelloWorld from './component/helloWorld';
 import './index.css';
 
 
-
 const Body = () => {
     return(
         <div id="Body">
             <Header/>
+            <Maket/>
             <Section/>
             <Footer/>
+        </div>
+    )
+}
+
+const Maket = () => {
+    return (
+        <div id="banniere_image">
+            <div id="banniere_description">
+                maquettes de la clinique
+                <a href= {require("./image/\maquette des cliniques.png")} class="bouton_rouge" target="_blank">Voir l'article <img src={require("./image/flecheblanchedroite.png")} alt="" /></a>
+            </div>
         </div>
     )
 }
@@ -18,12 +29,10 @@ const Body = () => {
 const Header = () =>{
     return(
         <div id="header">
-                <div id="logo">
-                    <img src="image/\Logo de clinique.JPG" alt="Logo de la clinique" />    
-                </div>
-                <h1>Clinique Matunzo Bora</h1>
-                <h2>L'excellence et les soins de qualité</h2>
-            
+            <div id="logo">
+                    <img src={require("././image/\Logo de clinique.JPG")} alt="Logo de la clinique" />
+                    <p>Clinique Matunzo Bora<br/>L'excellence et les soins de qualité</p>
+            </div>
             <div class="topnav">
                 <a href="#">Accueil</a>
                 <a href="#">horaire</a>
@@ -34,13 +43,6 @@ const Header = () =>{
                     <i class="fa fa-bars"></i>
                 </a>
             </div>
-
-            <div id="banniere_image">
-                <div id="banniere_description">
-                    maquettes de la clinique
-                    <a href="image\maquette des cliniques.png" class="bouton_rouge" target="_blank">Voir l'article <img src="image/flecheblanchedroite.png" alt="" /></a>
-                </div>
-            </div>
         </div>
         
     );
@@ -48,19 +50,19 @@ const Header = () =>{
 
 const Section = () =>{
     return (
-        <section>
+        <section class="data">
             <article>
-                <h1><img src="image/ico_epingle.png" alt="Catégorie voyage" class="ico_categorie" />Je suis un grand voyageur</h1>
+                <h1><img  alt="Catégorie voyage" class="ico_categorie" />Je suis un grand voyageur</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec sagittis massa. Nulla facilisi. Cras id arcu lorem, et semper purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel enim mi, in lobortis sem. Vestibulum luctus elit eu libero ultrices id fermentum sem sagittis. Nulla imperdiet mauris sed sapien dignissim id aliquam est aliquam. Maecenas non odio ipsum, a elementum nisi. Mauris non erat eu erat placerat convallis. Mauris in pretium urna. Cras laoreet molestie odio, consequat consequat velit commodo eu. Integer vitae lectus ac nunc posuere pellentesque non at eros. Suspendisse non lectus lorem.</p>
                 <p>Vivamus sed libero nec mauris pulvinar facilisis ut non sem. Quisque mollis ullamcorper diam vel faucibus. Vestibulum sollicitudin facilisis feugiat. Nulla euismod sodales hendrerit. Donec quis orci arcu. Vivamus fermentum magna a erat ullamcorper dignissim pretium nunc aliquam. Aenean pulvinar condimentum enim a dignissim. Vivamus sit amet lectus at ante adipiscing adipiscing eget vitae felis. In at fringilla est. Cras id velit ut magna rutrum commodo. Etiam ut scelerisque purus. Duis risus elit, venenatis vel rutrum in, imperdiet in quam. Sed vestibulum, libero ut bibendum consectetur, eros ipsum ultrices nisl, in rutrum diam augue non tortor. Fusce nec massa et risus dapibus aliquam vitae nec diam.</p>
                 <p>Phasellus ligula massa, congue ac vulputate non, dignissim at augue. Sed auctor fringilla quam quis porttitor. Praesent vitae dignissim magna. Pellentesque quis sem purus, vel elementum mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas consectetur euismod urna. In hac habitasse platea dictumst. Quisque tincidunt porttitor vestibulum. Ut iaculis, lacus at molestie lacinia, ipsum mi adipiscing ligula, vel mollis sem risus eu lectus. Nunc elit quam, rutrum ut dignissim sit amet, egestas at sem.</p>
             </article>
-            <aside>
+            <aside>{}
                 <h1>Nos valeur </h1>
-                <img src="../src/image/bulle.png" alt="" id="fleche_bulle" />
+                <img src={require("./image/bulle.png")} alt="" id="fleche_bulle" />
                 <p>Laisse-moi le temps de me présenter&nbsp;: je m'appelle Zozor, je suis né un 23 novembre 2005.</p>
                 <p>Bien maigre, n'est-ce pas ? C'est pourquoi, aujourd'hui, j'ai décidé d'écrire ma biographie afin que vous sachiez qui je suis réellement.</p>
-                <p id="reseaux_sociaux"><img src="image/facebook.png" alt="Facebook" /><img src="image/twitter.png" alt="Twitter" /><img src="image/vimeo.png" alt="Vimeo" /><img src="image/flickr.png" alt="Flickr" /><img src="image/rss.png" alt="RSS" /></p>
+                <p id="reseaux_sociaux"><img src= {require("./image/facebook.png")} alt="Facebook" /><img src= {require("./image/twitter.png")} alt="Twitter" /><img src={require("./image/vimeo.png")} alt="Vimeo" /><img src= {require("./image/flickr.png")} alt="Flickr" /><img src= {require("./image/rss.png")} alt="RSS" /></p>
             </aside>
         </section>
     )
@@ -71,8 +73,8 @@ const Footer = () =>{
         <footer>
             <div id="mes_photos">
                 <h1>Fondateur</h1>
-                <p><a href="image/dieudonné agrandi.jpg" target="_blank" ><img src="image/photo dieudonné.jpg" alt="Dieudonné"  />Dieudonné Biheye </a></p>
-                <p><a href="image/john agrandi.JPG" target="_blank" ><img src="image/photo john.jpg"  alt="john"/>John Kivukuto</a></p>
+                <p><a href={require("./image/dieudonné agrandi.jpg")} target="_blank" ><img src={require("./image/photo dieudonné.jpg")} alt="Dieudonné"  />Dieudonné Biheye </a></p>
+                <p><a href={require("./image/john agrandi.JPG")} target="_blank" ><img src={require("./image/photo john.JPG")}  alt="john"/>John Kivukuto</a></p>
             </div>
             <div id="mes_amis">
                 <h1>Partenaires</h1>
